ba29205bda6cb5229e4d88843b654696fea7002d
yun1122 <yun1122@outlook.com>
2022-07-05 22:37:47 +0800

    代码初始化

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h2>{{ $store.state.name }}</h2>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
+    <el-button>登录</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "App",
+  components: {}
+})
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore } from "vuex"
+
+const store = createStore({
+  state() {
+    return {
+      name: "jly"
+    }
+  }
+})
+
+export default store
